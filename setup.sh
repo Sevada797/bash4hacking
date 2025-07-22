90d18d41f836d269fd8375c986a6cdaf96282452
Sevada <sevada@Someofmydomains.com>
2025-07-22 21:43:58 +0400

    Added setup for zsh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Determine which RC file to use
-if [ -n "$ZSH_VERSION" ]; then
+# Determine which RC file to use (main check: $SHELL)
+SHELL_NAME=$(basename "$SHELL")
+
+if [ "$SHELL_NAME" = "zsh" ]; then
+    RC_FILE="$HOME/.zshrc"
+elif [ "$SHELL_NAME" = "bash" ]; then
+    RC_FILE="$HOME/.bashrc"
+elif [ -n "$ZSH_VERSION" ]; then
     RC_FILE="$HOME/.zshrc"
 elif [ -n "$BASH_VERSION" ]; then
     RC_FILE="$HOME/.bashrc"
 else
-    # Fallback: check current shell manually
-    SHELL_NAME=$(basename "$SHELL")
-    if [ "$SHELL_NAME" = "zsh" ]; then
-        RC_FILE="$HOME/.zshrc"
-    else
-        RC_FILE="$HOME/.bashrc"
-    fi
+    # Default fallback to .bashrc
+    RC_FILE="$HOME/.bashrc"
 fi
+
 
 IMPORT_MARK="# IMPORT for Bash4hacking"
 SRC_DIR="$(pwd)/src"
